f5ac4b3da1c0a44ee89f6eb5ade23323879b74fe
yipeng <1357933695@qq.com>
2019-08-21 16:24:48 +0800

    毕设项目重复了

--- 毕设项目/ssm/src/com/digital/dao/Shopping_trolleyInfoAddDAO.java
+++
@@ -1,9 +0,0 @@
-package com.digital.dao;
-
-import java.util.List;
-import com.digital.pojo.Shopping_trolleyInfoAdd;
-
-public interface Shopping_trolleyInfoAddDAO {
-
-//	public List<Shopping_trolleyInfoAdd> getShopping_trolleyInfo(Shopping_trolleyInfoAdd trolley);
-}
